965e3c53e1d2701bfdac848cf13a5ad287570077
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-02-25 18:27:02 -0500

    -

--- src/algorithm/linkedlist/linkedlist.js
+++ src/algorithm/linkedlist/linkedlist.js
@@ -1,11 +1,50 @@
-// const n1 = {
-//     data: 100
-// }
+class Node {
+    constructor(value){
+        this.value = value
+        this.next = null
+    }
+}
 
-// const n2 = {
-//     deata: 200
-// }
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value)
+        this.head = newNode
+        this.tail = this.head
+        this.length = 1
+    }
 
-// n1.next = n2
+    push(value) { // O(1) give next to tail node.
+        const newNode = new Node(value)
+        if (!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        } else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this
+    }
+    pop() { // O(n) start from head to find the previous node of tail node.
+        // we need pre, temp.
+        if (!this.head) return undefined            
 
-// console.log(n1)
+        let temp = this.head
+        let pre = this.head
+
+        while (!temp.next) {
+            pre = temp
+            temp = temp.next
+
+            if (!temp.next) {
+                this.tail = pre
+            }
+        }
+
+        this.length--
+        return this
+    }
+}
+
+let myLinkedList = new LinkedList(7)
+myLinkedList.push(4)
