1cf246fce9aae18e08d160356322ffa32fca0ad5
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-02-25 17:53:06 -0500

    -

--- src/algorithm/linkedlist/linkedlist.js
+++ src/algorithm/linkedlist/linkedlist.js
@@ -1,11 +1,11 @@
-const n1 = {
-    data: 100
-}
+// const n1 = {
+//     data: 100
+// }
 
-const n2 = {
-    deata: 200
-}
+// const n2 = {
+//     deata: 200
+// }
 
-n1.next = n2
+// n1.next = n2
 
-console.log(n1)
+// console.log(n1)
