0f90ef8f498c7bcf71e9717ab8d734b250696aeb
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-03-03 11:27:23 -0500

    -

--- src/data_structure/hash/hash.js
+++ src/data_structure/hash/hash.js
@@ -10,4 +10,39 @@ class HashTable {
         }
         return hash
     }
+
+    set(key, value) {
+        let index = this._hash(key)
+        if (!this.dataMap[index]) {
+            this.dataMap[index] = []
+        }        
+        this.dataMap[index].push([key, value])
+
+        return this
+    }
+
+    get(key) {
+        let index = this._hash(key)
+        if (this.dataMap[index]) {
+            for (let i = 0; i < this.dataMap[index].length; i++) {
+                if (this.dataMap[index][i][0] === key) {
+                    return this.dataMap[index][i][1]
+                }                
+            }   
+        }
+        return undefined
+    }
+
+    keys() {
+        let allKeys = []
+        for (let i = 0; i < this.dataMap.length; i++) {
+            if (this.dataMap[i]) {
+                for (let j = 0; j < this.dataMap[i].length; j++) {
+                    allKeys.push(this.dataMap[i][j][0])                    
+                }
+            }                    
+        }
+        return allKeys 
+    }
+
 }
