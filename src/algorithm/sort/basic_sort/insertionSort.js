489c0cb445d8154318257e4cd287448aff640b43
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-03-08 18:27:45 -0500

    -

--- src/algorithm/sort/basic_sort/insertionSort.js
+++ src/algorithm/sort/basic_sort/insertionSort.js
@@ -2,5 +2,9 @@ function insertionSort(array) {
     let temp
     for (let i = 1; i < array.length; i++) {
         temp = array[i];
+        for(var j = i - 1; array[j] > temp; j--) { // use var because we are going to use it outside of forloop
+            array[j+1] = array[j]
+        }
+        array[j+1] = temp
     }
 }
